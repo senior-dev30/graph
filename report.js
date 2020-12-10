ef2a6a6a40fbfdc00550d657e8996872a5df050c
yamamoto-ichiro <yamamoto.ichiro92@gmail.com>
2020-12-11 02:59:37 +0900

    add regression line

--- report.js
+++ report.js
@@ -9,7 +9,7 @@ var svg = d3.select("body").append("svg")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 var x = d3.scaleLog()
-    .domain([0.1, 100])
+    .domain([0.01, 100])
     .range([0, width]);
 
 var y = d3.scaleLinear()
@@ -43,3 +43,38 @@ svg.append("text")
     .attr("dy", "1em")
     .style("text-anchor", "middle")
     .text("Binding function");     
+
+d3.json("data.json")
+.then((data)=> {
+    console.log(data);
+
+    svg.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", function (d) { return x(d.concentration); } )
+    .attr("cy", function (d) { return y(d.binding); } )
+    .attr("r", 3)
+    .style("fill", "#111111");
+
+    var lineGenerator = d3.line()
+    .x(d => x(d[0]))
+    .y(d => y(d[1]));
+
+    var regressionGenerator = d3.regressionPoly()
+        .x(d => d.concentration)
+        .y(d => d.binding)
+        .domain([0.01, 100])(data);
+
+    svg.append("path")
+      .datum(regressionGenerator)
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("stroke-linejoin", "round")
+      .attr("stroke-linecap", "round")
+      .attr("d", lineGenerator);
+    
+    console.log(regressionGenerator);
+});
